ae5618a0eb65764bcc6618bfd3575844746e2ac1
Tati <eytati050@hotmail.com>
2017-03-30 18:02:11 -0600

    Se agrego el primer ejecicio y primeros metodos del segundo

--- Practica_Tatiana/Practica.py
+++ Practica_Tatiana/Practica.py
@@ -1,7 +1,4 @@
 '''
-1. Crear  y alimentar un vector, 1000 elementos tipo double.
-    a. Crear un método que calcule las raíces cuadradas de cada entrada del arreglo
-    b. Crear un método que reste n unidades a cada entrada del arreglo
 2. Crear y alimentar un vector de números enteros generados aleatoriamente.
     a. Obtener un rango de elementos de dicho vector, donde el rango está dado por el índice a y b tomando en cuenta que rango no debe superar las dimensiones del arreglo.
     b. Obtener los números primos que se encuentra en el arreglo
@@ -19,48 +16,88 @@
        Entrada: [a,b,n,m,x,]
        Salida: [b,n,m,x,]
 '''
+import random
+import math
 
+class Punto1_Vector_de_double ():
 
-class ejercicios ():
-    matriz = []
-    #def __init__(self, matriz):
-     #self.matriz = matriz
+#--------------------------------------Crear Vector de double----------------------------------------------------------#
+    vector_doubles = []
 
-    def rellenar_matriz(self, largo):
+#--------------------------------------------Rellenar vector de double-------------------------------------------------#
+    def rellenar_vector_doubles(self, largo):
         for linea in range(largo):
-            self.matriz.append([])
-            for columna in range(largo):
-                self.matriz[linea].append("Hola")
-
-    def matriz_double(self, matriz):
-        self.rellenar_matriz(1000)
-        # Rellenar matriz
-        # Recorrer matriz
-        # restar n numero de matriz
-
-    def matriz_numeros_aleatoreos(self):
-        print("hola")
-        #Rellenar matriz
-        #Rango de a-b -busque nuemeros
-        #Numeros primos
-        #pares e impares => return indice y el numero
-        #Invertir arreglo
-        #largo del arreglo
-
-    def eliminar_elemento(self):
-        print("hola")
-        #Eliminar elemento inicial
-
-    def impresion_matriz(self):
-        cadena_de_sring = ''
-        for linea in range(len(self.matriz)):
-            for columna in range(len(self.matriz[linea])):
-                cadena_de_sring += str(self.matriz[linea][columna]) + ' '
-            cadena_de_sring += '\n'
-        return cadena_de_sring
+            self.vector_doubles.append(random.uniform(0.0, 100.0))
 
 
-a = ejercicios()
-a.impresion_matriz()
-a.matriz_double(a.matriz)
-a.impresion_matriz()
+#-----------------------------------------Recorrer calcular las raices-------------------------------------------------#
+    def recorrer_vector_de_doubles_calculo_raices(self):
+        cadena_de_resultados = ''
+        for linea in range(len(self.vector_doubles)):
+            resultado_de_raiz = math.sqrt(self.vector_doubles[linea])
+            cadena_de_resultados += str(resultado_de_raiz) + ' '
+        return cadena_de_resultados
+
+#----------------------------------Resta un numero a todas las posiciones----------------------------------------------#
+    def recorrer_vector_de_doubles_restar_un_numero(self, numero):
+        cadena_de_resultados = ''
+        for linea in range(len(self.vector_doubles)):
+            resultado_de_la_resta = self.vector_doubles[linea] - numero
+            cadena_de_resultados += str(resultado_de_la_resta) + ' '
+        return cadena_de_resultados
+
+#------------------------------------------Imprimir vector de doubles--------------------------------------------------#
+    def impresion_de_vector_double(self):
+        cadena_de_texto = ''
+        for linea in range(len(self.vector_doubles)):
+            cadena_de_texto += str(self.vector_doubles[linea]) + ' '
+        return cadena_de_texto
+
+
+#------------------------------------------------Llamado de metodos anteriores-----------------------------------------#
+    def vector_double(self):
+        self.rellenar_vector_doubles(10)
+        print(self.recorrer_vector_de_doubles_calculo_raices())
+        print(self.recorrer_vector_de_doubles_restar_un_numero(-5))
+        print(self.impresion_de_vector_double())
+
+class Punto2_vector_de_enteros():
+#--------------------------------------Crear Vector de enteros---------------------------------------------------------#
+    vector_enteros = []
+
+#----------------------------------Rellenar vector de enteros----------------------------------------------------------#
+    def rellenar_vector_de_enteros(self, largo):
+            for linea in range(largo):
+                self.vector_enteros.append(random.randint(0, 100))
+
+#-----------------------------------Impirmir el vector de enteros------------------------------------------------------#
+    def impresion_de_vector_enteros(self):
+        cadena_de_enteros = ''
+        for linea in range(len(self.vector_enteros)):
+            cadena_de_enteros += str(self.vector_enteros[linea]) + ' '
+        return cadena_de_enteros
+
+#-------------------------------Recorrer y obtener los numeros de un rango---------------------------------------------#
+    def recorrer_vector_de_enteros_obtener_numeros_rango(self, numero1, numero2):
+        cadena_de_resultados = ''
+        for linea in range(len(self.vector_enteros)):
+           valor_de_casilla_actual = self.vector_enteros[linea]
+           if valor_de_casilla_actual>numero1 and valor_de_casilla_actual<numero2:
+            cadena_de_resultados += str(valor_de_casilla_actual)+ ' '
+        return cadena_de_resultados
+#------------------------------------------------Llamado de metodos anteriores-----------------------------------------#
+    def vector_numeros_aleatoreos(self):
+        self.rellenar_vector_de_enteros(10)
+        print (self.recorrer_vector_de_enteros_obtener_numeros_rango(0,20))
+        # Numeros primos
+        # pares e impares => return indice y el numero
+        #invertir arreglo
+        #obtener largo
+        #obtener elemento del medio
+        print(self.impresion_de_vector_enteros())
+
+#Eliminar elemento ejercicio #3
+a = Punto1_Vector_de_double()
+a.vector_double()
+b = Punto2_vector_de_enteros()
+b.vector_numeros_aleatoreos()
