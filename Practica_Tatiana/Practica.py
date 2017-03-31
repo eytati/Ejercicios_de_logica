a8b5a8272909785212683a1032800dbea1d83818
Tati <eytati050@hotmail.com>
2017-03-31 15:20:37 -0600

    Se agregaron los  ejercios de la parte 2

--- Practica_Tatiana/Practica.py
+++ Practica_Tatiana/Practica.py
@@ -1,21 +1,3 @@
-'''
-2. Crear y alimentar un vector de números enteros generados aleatoriamente.
-    a. Obtener un rango de elementos de dicho vector, donde el rango está dado por el índice a y b tomando en cuenta que rango no debe superar las dimensiones del arreglo.
-    b. Obtener los números primos que se encuentra en el arreglo
-    c. Obtener los números pares e impares
-            i. Se devuelve el índice
-            ii. Se devuelve el numero
-    d. Invertir arreglo
-            i. Se invierte el orden del arreglo, esto es si se tiene el arreglo: [a,b,c,d,r,f,g,] la salida de invertir seria: [g,f,r,d,c,d,a]
-    e. Obtener el Largo de un Arreglo:
-            i. [g,f,r,d,c,d,a] -> 6
-            ii. [] -> 0
-    f. Obtener el elemento medio de un arreglo
-            i. Devuelve la posición largo/2 del arreglo en caso de no ser división entera devuelve la posición largo-1/2
-3. Eliminar el primer elemento un vector:
-       Entrada: [a,b,n,m,x,]
-       Salida: [b,n,m,x,]
-'''
 import random
 import math
 
@@ -57,9 +39,12 @@ class Punto1_Vector_de_double ():
 #------------------------------------------------Llamado de metodos anteriores-----------------------------------------#
     def vector_double(self):
         self.rellenar_vector_doubles(10)
-        print(self.recorrer_vector_de_doubles_calculo_raices())
-        print(self.recorrer_vector_de_doubles_restar_un_numero(-5))
-        print(self.impresion_de_vector_double())
+        print('-------------------------Calculos de raices-------------------------------------')
+        print(self.recorrer_vector_de_doubles_calculo_raices()+'\n')
+        print('-------------------------Restar un numero---------------------------------------')
+        print(self.recorrer_vector_de_doubles_restar_un_numero(-5)+'\n')
+        print('-------------------------Vector completo----------------------------------------')
+        print(self.impresion_de_vector_double()+'\n')
 
 class Punto2_vector_de_enteros():
 #--------------------------------------Crear Vector de enteros---------------------------------------------------------#
@@ -70,12 +55,6 @@ class Punto2_vector_de_enteros():
             for linea in range(largo):
                 self.vector_enteros.append(random.randint(0, 100))
 
-#-----------------------------------Impirmir el vector de enteros------------------------------------------------------#
-    def impresion_de_vector_enteros(self):
-        cadena_de_enteros = ''
-        for linea in range(len(self.vector_enteros)):
-            cadena_de_enteros += str(self.vector_enteros[linea]) + ' '
-        return cadena_de_enteros
 
 #-------------------------------Recorrer y obtener los numeros de un rango---------------------------------------------#
     def recorrer_vector_de_enteros_obtener_numeros_rango(self, numero1, numero2):
@@ -85,18 +64,91 @@ class Punto2_vector_de_enteros():
            if valor_de_casilla_actual>numero1 and valor_de_casilla_actual<numero2:
             cadena_de_resultados += str(valor_de_casilla_actual)+ ' '
         return cadena_de_resultados
+
+#-------------------------------Recorrer y obtener los numeros primos y sus indices------------------------------------#
+    def recorrer_vector_de_enteros_obtener_numeros_primos(self):
+        cadena_de_resultados = ''
+        indice_divisor= 0
+        for linea in range(len(self.vector_enteros)):
+            valor_de_casilla_actual = self.vector_enteros[linea]
+            divisor= 0
+            primo = True
+            if valor_de_casilla_actual is 2:
+                primo = True
+
+            elif valor_de_casilla_actual % 2 is 0:
+                primo = False
+                divisor = 2
+            else:
+                mediaRelativa = int((valor_de_casilla_actual - 1) / 2)
+                for contador in range(3, mediaRelativa):
+                    if valor_de_casilla_actual % contador is 0:
+                        primo = False
+                        divisor = contador
+                        break
+            cadena_de_resultados += 'Numero: '+ str(valor_de_casilla_actual)+' Primo: '+ str(primo)+ ' Indice: '+str(divisor)+ '  '
+        return cadena_de_resultados
+
+#------------------------------------Recorrer y obtener pares e impares------------------------------------------------#
+    def recorrer_vector_de_enteros_numeros_pares(self):
+        cadena_de_resultados = ''
+        for linea in range(len(self.vector_enteros)):
+            valor_de_casilla_actual = self.vector_enteros[linea]
+            if valor_de_casilla_actual %2 is 0:
+                cadena_de_resultados += 'Par: '+str(valor_de_casilla_actual)+ ' '
+            else:
+                cadena_de_resultados += 'Impar: ' + str(valor_de_casilla_actual) + ' '
+        return cadena_de_resultados
+
+#------------------------------------Recorrer e invertir el orden------------------------------------------------------#
+    def recorrer_vector_de_enteros_invertir_orden(self):
+        cadena_de_resultados = ''
+        inversor = len(self.vector_enteros)-1
+        for linea in range(len(self.vector_enteros)):
+            valor_de_casilla_actual = self.vector_enteros[inversor]
+            cadena_de_resultados += str(valor_de_casilla_actual)+ ' '
+            inversor -= 1
+        return cadena_de_resultados
+
+#-----------------------------------Recorrer y obtener valor del medio--------------------------------------------------#
+    def recorre_vector_de_enteros_valor_del_medio(self):
+        cadena_de_enteros = ''
+        largo_de_vector = int(len(self.vector_enteros))
+        if largo_de_vector % 2  is 0:
+            largo_de_vector = int(largo_de_vector/2)
+            cadena_de_enteros += str(self.vector_enteros[largo_de_vector - 1]) + ' '
+            cadena_de_enteros += str(self.vector_enteros[largo_de_vector])
+        else:
+            largo_de_vector = int(((largo_de_vector-1)/2))
+            cadena_de_enteros += str(self.vector_enteros[largo_de_vector]) + ' '
+
+        return cadena_de_enteros
+
+#-----------------------------------------Impresion de Vector----------------------------------------------------------#
+    def impresion_de_vector_enteros(self):
+        cadena_de_resultados = ''
+        for linea in range(len(self.vector_enteros)):
+            cadena_de_resultados += str(self.vector_enteros[linea])+ ' '
+        return cadena_de_resultados
+
 #------------------------------------------------Llamado de metodos anteriores-----------------------------------------#
     def vector_numeros_aleatoreos(self):
-        self.rellenar_vector_de_enteros(10)
-        print (self.recorrer_vector_de_enteros_obtener_numeros_rango(0,20))
-        # Numeros primos
-        # pares e impares => return indice y el numero
-        #invertir arreglo
+        self.rellenar_vector_de_enteros(9)
+        print('-------------------------Numeros del rango--------------------------------------')
+        print (self.recorrer_vector_de_enteros_obtener_numeros_rango(0,20)+'\n')
+        print('--------------------------Numeros primos----------------------------------------')
+        print(self.recorrer_vector_de_enteros_obtener_numeros_primos()+'\n')
+        print('--------------------------Numeros pares e impares-------------------------------')
+        print(self.recorrer_vector_de_enteros_numeros_pares() + '\n')
+        print('-------------------------Invertir el orden--------------------------------------')
+        print(self.recorrer_vector_de_enteros_invertir_orden()+'\n')
         #obtener largo
-        #obtener elemento del medio
+        print('-------------------------Valor del medio----------------------------------------')
+        print(self.recorre_vector_de_enteros_valor_del_medio()+'\n')
+        print('-------------------------Vector completo----------------------------------------')
         print(self.impresion_de_vector_enteros())
 
-#Eliminar elemento ejercicio #3
+
 a = Punto1_Vector_de_double()
 a.vector_double()
 b = Punto2_vector_de_enteros()
